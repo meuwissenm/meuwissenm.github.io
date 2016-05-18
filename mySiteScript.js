cfd200a1d707175bb3273e114d5b79457eb12467
meuwissenm <meuwissen26@gmail.com>
2016-05-17 22:26:29 -0500

    Update mySiteScript.js

--- mySiteScript.js
+++ mySiteScript.js
@@ -8,4 +8,34 @@ document.getElementById("silcAdminImage").onclick = function() {
         document.getElementById("silcAdminImage").style.width = "0%";
 
 }
+document.getElementById("showScene1").onclick = function() {
+    document.getElementById("scene1").style.visibility = "visible";
+        document.getElementById("scene1").style.width = "100%";
+
+}
+document.getElementById("scene1").onclick = function() {
+    document.getElementById("scene1").style.visibility = "hidden";
+        document.getElementById("scene1").style.width = "0%";
+
+}
+document.getElementById("showScene2").onclick = function() {
+    document.getElementById("scene2").style.visibility = "visible";
+        document.getElementById("scene2").style.width = "100%";
+
+}
+document.getElementById("scene2").onclick = function() {
+    document.getElementById("scene2").style.visibility = "hidden";
+        document.getElementById("scene2").style.width = "0%";
+
+}
+document.getElementById("showScene3").onclick = function() {
+    document.getElementById("scene3").style.visibility = "visible";
+        document.getElementById("scene3").style.width = "100%";
+
+}
+document.getElementById("scene3").onclick = function() {
+    document.getElementById("scene3").style.visibility = "hidden";
+        document.getElementById("scene3").style.width = "0%";
+
+}
 
