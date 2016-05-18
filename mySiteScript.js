78f1ec7f7e5201cdb3a818a149a3ab188826ae64
meuwissenm <meuwissen26@gmail.com>
2016-05-17 22:34:43 -0500

    Update mySiteScript.js

--- mySiteScript.js
+++ mySiteScript.js
@@ -1,46 +1,50 @@
 document.getElementById("showScreenshot").onclick = function() {
     document.getElementById("silcAdminImage").style.visibility = "visible";
         document.getElementById("silcAdminImage").style.width = "100%";
+                    document.getElementById("showScreenshot").style.visibility = "hidden";
 
 }
 document.getElementById("silcAdminImage").onclick = function() {
     document.getElementById("silcAdminImage").style.visibility = "hidden";
         document.getElementById("silcAdminImage").style.width = "0%";
-            document.getElementById("showScreenshot").style.visibility = "hidden";
+            document.getElementById("showScreenshot").style.visibility = "visible";
 
 }
 document.getElementById("showScene1").onclick = function() {
     document.getElementById("scene1").style.visibility = "visible";
         document.getElementById("scene1").style.width = "100%";
+        document.getElementById("showScene1").style.visibility = "hidden";
 
 }
 document.getElementById("scene1").onclick = function() {
     document.getElementById("scene1").style.visibility = "hidden";
         document.getElementById("scene1").style.width = "0%";
-              document.getElementById("showScene1").style.visibility = "hidden";
+              document.getElementById("showScene1").style.visibility = "visible";
         
 
 }
 document.getElementById("showScene2").onclick = function() {
     document.getElementById("scene2").style.visibility = "visible";
         document.getElementById("scene2").style.width = "100%";
+                 document.getElementById("showScene2").style.visibility = "hidden";
 
 }
 document.getElementById("scene2").onclick = function() {
     document.getElementById("scene2").style.visibility = "hidden";
         document.getElementById("scene2").style.width = "0%";
-              document.getElementById("showScene2").style.visibility = "hidden";
+              document.getElementById("showScene2").style.visibility = "visible";
 
 }
 document.getElementById("showScene3").onclick = function() {
     document.getElementById("scene3").style.visibility = "visible";
         document.getElementById("scene3").style.width = "100%";
+                      document.getElementById("showScene3").style.visibility = "hidden";
 
 }
 document.getElementById("scene3").onclick = function() {
     document.getElementById("scene3").style.visibility = "hidden";
         document.getElementById("scene3").style.width = "0%";
-              document.getElementById("showScene3").style.visibility = "hidden";
+              document.getElementById("showScene3").style.visibility = "visible";
 
 }
 
