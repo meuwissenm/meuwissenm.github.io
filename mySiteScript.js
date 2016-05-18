00022c706ee0fd49d65f6e27a676e2a994d54216
meuwissenm <meuwissen26@gmail.com>
2016-05-17 19:44:07 -0500

    Update mySiteScript.js

--- mySiteScript.js
+++ mySiteScript.js
@@ -1,3 +1,5 @@
 document.getElementById("showScreenshot").onclick = function() {
     document.getElementById("silcAdminImage").style.visibility = "visible";
+        document.getElementById("silcAdminImage").style.width = "100%";
+
 }
