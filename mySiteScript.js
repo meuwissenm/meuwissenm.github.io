870bb1b22cde08164f57379d11a5a3de615d119d
meuwissenm <meuwissen26@gmail.com>
2016-05-23 14:24:52 -0500

    Update mySiteScript.js

--- mySiteScript.js
+++ mySiteScript.js
@@ -10,41 +10,5 @@ document.getElementById("silcAdminImage").onclick = function() {
             document.getElementById("showScreenshot").style.visibility = "visible";
 
 }
-document.getElementById("showScene1").onclick = function() {
-    document.getElementById("scene1").style.visibility = "visible";
-        document.getElementById("scene1").style.width = "100%";
-        document.getElementById("showScene1").style.visibility = "hidden";
 
-}
-document.getElementById("scene1").onclick = function() {
-    document.getElementById("scene1").style.visibility = "hidden";
-        document.getElementById("scene1").style.width = "0%";
-              document.getElementById("showScene1").style.visibility = "visible";
-        
-
-}
-document.getElementById("showScene2").onclick = function() {
-    document.getElementById("scene2").style.visibility = "visible";
-        document.getElementById("scene2").style.width = "100%";
-                 document.getElementById("showScene2").style.visibility = "hidden";
-
-}
-document.getElementById("scene2").onclick = function() {
-    document.getElementById("scene2").style.visibility = "hidden";
-        document.getElementById("scene2").style.width = "0%";
-              document.getElementById("showScene2").style.visibility = "visible";
-
-}
-document.getElementById("showScene3").onclick = function() {
-    document.getElementById("scene3").style.visibility = "visible";
-        document.getElementById("scene3").style.width = "100%";
-                      document.getElementById("showScene3").style.visibility = "hidden";
-
-}
-document.getElementById("scene3").onclick = function() {
-    document.getElementById("scene3").style.visibility = "hidden";
-        document.getElementById("scene3").style.width = "0%";
-              document.getElementById("showScene3").style.visibility = "visible";
-
-}
 
