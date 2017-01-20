ab17e52973e32f4013b05f5ee50979fd4f20aea6
meuwissenm <meuwissen26@gmail.com>
2017-01-19 18:47:26 -0600

    Update mySiteScript.js

--- mySiteScript.js
+++ mySiteScript.js
@@ -2,28 +2,28 @@ document.getElementById("showScreenshot").onclick = function() {
     document.getElementById("silcAdminImage").style.visibility = "visible";
         document.getElementById("silcAdminImage").style.width = "100%";
                     document.getElementById("showScreenshot").style.visibility = "hidden";
-                     document.getElementById("plusSignProg").style.height = "0%";
+                     document.getElementById("plusSignProg").style.width = "0%";
 
 }
 document.getElementById("silcAdminImage").onclick = function() {
     document.getElementById("silcAdminImage").style.visibility = "hidden";
         document.getElementById("silcAdminImage").style.width = "0%";
             document.getElementById("showScreenshot").style.visibility = "visible";
-            document.getElementById("plusSignProg").style.height = "3%";
+            document.getElementById("plusSignProg").style.width = "3%";
 
 }
 document.getElementById("showScreenshotHandoff").onclick = function() {
     document.getElementById("HandoffScreenshot").style.visibility = "visible";
         document.getElementById("HandoffScreenshot").style.width = "100%";
               document.getElementById("showScreenshotHandoff").style.visibility = "hidden";
-                  document.getElementById("plusSignProgTwo").style.height = "0%";
+                  document.getElementById("plusSignProgTwo").style.width = "0%";
 
 }
 document.getElementById("HandoffScreenshot").onclick = function() {
     document.getElementById("HandoffScreenshot").style.visibility = "hidden";
         document.getElementById("HandoffScreenshot").style.width = "0%";
               document.getElementById("showScreenshotHandoff").style.visibility = "visible";
-                 document.getElementById("plusSignProgTwo").style.height = "3%";
+                 document.getElementById("plusSignProgTwo").style.width = "3%";
 
 }
 
