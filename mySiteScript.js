945d7af99a4a53643be31d8a8b428f5a973b69ea
meuwissenm <meuwissen26@gmail.com>
2017-01-21 17:05:17 -0600

    Update mySiteScript.js

--- mySiteScript.js
+++ mySiteScript.js
@@ -26,4 +26,48 @@ document.getElementById("HandoffScreenshot").onclick = function() {
                  document.getElementById("plusSignProgTwo").style.width = "3%";
 
 }
+document.getElementById("showScreenshotThree").onclick = function() {
+    document.getElementById("contextExtImage").style.visibility = "visible";
+        document.getElementById("contextExtImage").style.width = "100%";
+              document.getElementById("showScreenshotThree").style.visibility = "hidden";
+                  document.getElementById("plusSignProgThree").style.width = "0%";
+
+}
+document.getElementById("contextExtImage").onclick = function() {
+    document.getElementById("contextExtImage").style.visibility = "hidden";
+        document.getElementById("contextExtImage").style.width = "0%";
+              document.getElementById("showScreenshotThree").style.visibility = "visible";
+                 document.getElementById("plusSignProgThree").style.width = "3%";
+
+}
+document.getElementById("showCode").onclick = function() {
+    document.getElementById("musicSearchImage").style.visibility = "visible";
+        document.getElementById("musicSearchImage").style.width = "100%";
+              document.getElementById("showCode").style.visibility = "hidden";
+                  document.getElementById("plusSignProgFour").style.width = "0%";
+
+}
+document.getElementById("musicSearchImage").onclick = function() {
+    document.getElementById("musicSearchImage").style.visibility = "hidden";
+        document.getElementById("musicSearchImage").style.width = "0%";
+              document.getElementById("showCode").style.visibility = "visible";
+                 document.getElementById("plusSignProgFour").style.width = "3%";
+
+}
+document.getElementById("showCode").onclick = function() {
+    document.getElementById("menuSearchImage").style.visibility = "visible";
+        document.getElementById("menuSearchImage").style.width = "100%";
+              document.getElementById("showCodeTwo").style.visibility = "hidden";
+                  document.getElementById("plusSignProgFive").style.width = "0%";
+
+}
+document.getElementById("menuSearchImage").onclick = function() {
+    document.getElementById("menuSearchImage").style.visibility = "hidden";
+        document.getElementById("menuSearchImage").style.width = "0%";
+              document.getElementById("showCodeTwo").style.visibility = "visible";
+                 document.getElementById("plusSignProgFive").style.width = "3%";
+
+}
+
+
 
